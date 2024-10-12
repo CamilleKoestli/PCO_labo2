2c65a7c70c7f04be8282acc54f1c900cf7e9e373
CamilleKoestli <91135499+CamilleKoestli@users.noreply.github.com>
2024-10-12 11:52:01 +0200

    done: fct getPermutation

--- code/src/mythread.cpp
+++ code/src/mythread.cpp
@@ -2,15 +2,18 @@
 #include "mythread.h"
 #include <iostream>
 
-void bogosort(std::vector<int> seq, ThreadManager* pManager /* TODO */)
+void bogosort(std::vector<int> seq, ThreadManager *pManager /* TODO */)
 {
+
     // TODO
-   while(!pManager->finished){
+    while (!pManager->finished)
+    {
         // Génération d'une séquence aléatoire
         std::random_shuffle(seq.begin(), seq.end());
 
         // Vérification si la séquence est triée
-        if (std::is_sorted(seq.begin(), seq.end())){
+        if (std::is_sorted(seq.begin(), seq.end()))
+        {
             pManager->finished = true;
             return;
         }
@@ -18,4 +21,20 @@ void bogosort(std::vector<int> seq, ThreadManager* pManager /* TODO */)
 
     // Exemple de mise à jour de la barre de progression
     pManager->incrementPercentComputed((double)1);
+}
+
+std::vector<int> getPermutation(std::vector<int> list, int k)
+{
+    size_t n = list.size();
+    std::vector<int> permutation;
+    std::vector<int> availableElements = permutation;
+
+    for (size_t i = 0; i < n; i++){
+        int fact = factorial(n - 1 - i);
+        int index = k / fact;
+        permutation.push_back(availableElements[index]);
+        availableElements.erase(availableElements.begin() + index);
+        k = k % fact;
+    }
+    return permutation;
 }
